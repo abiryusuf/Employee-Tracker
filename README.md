13ab0acf115c554891b1d27b0d826b7fcbba7b51
abiryusuf <yusuf_ctg@yahoo.com>
2020-04-28 13:47:52 -0400

    update

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 ## Employee_Tracker
 
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as **C**ontent **M**anagement **S**ystems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
+
+## Table
+
+* department:
+
+id - INT PRIMARY KEY
+name - VARCHAR(30) to hold department name
+
+* role:
+
+id - INT PRIMARY KEY
+title - VARCHAR(30) to hold role title
+salary - DECIMAL to hold role salary
+department_id - INT to hold reference to department role belongs to department
+
+* employee:
+
+id - INT PRIMARY KEY
+first_name - VARCHAR(30) to hold employee first name
+last_name - VARCHAR(30) to hold employee last name
+role_id - INT to hold reference to role employee has
+manager_id - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+
+Build a command-line application that at a minimum allows the user to:
+
+  * Add departments, roles, employees
+
+  * View departments, roles, employees
+
+  * Update employee roles
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const inquirer = require("inquire");
+const inquirer = require("inquirer");
 
 //const consoleTB = require("console.table");
 
@@ -16,16 +16,150 @@ const connection = mysql.createConnection({
 
 //connect with server
 connection.connect(function(err){
-    if(err) throw err;
-    console.log("connected as id " + connection.threadId);
-    employeeView();
+    if(err) {
+        console.error("Error: " + err.stack);
+        return err;
+    }
+    console.log("connected as id: " + connection.threadId);
+   
+    startRun();
+   //employeeView();
 
-})
+});
 
-function employeeView(){
-    var query = "SELECT * FROM employee";
-    connection.query(query, function(err, res){
-        console.log(res);
-        connection.end();
+const viewOptions =[
+    "ViewEmployees",
+    "ViewDepartment",
+    "ViewRoles",
+    "UpdateEmployee",
+    "Exit"
+ ];
+
+ const updateOptions = [
+    "First Name",
+    "Last Name",
+    "Role",
+    "exit"
+];
+
+
+function startRun(){
+    inquirer
+    .prompt({
+        name:"action",
+        type: "list",
+        message: "What would you like to do ?",
+        choices: viewOptions
+    })
+    .then(function(answer){
+        switch(answer.action){
+            case viewOptions[0]:
+                employeeView();
+                break;
+
+            case viewOptions[2]:
+                departmentView();
+                break;
+
+            case viewOptions[2]:
+                roleView();
+                break; 
+
+            case viewOptions[3]:
+                updateEmployee();
+                break;
+
+            case updateOptions[4]:
+                connection.end();
+                break;       
+        }
     })
 }
+
+function employeeView() {
+    var query = "SELECT first_name, last_name, title, salary FROM employee ";
+    query += "LEFT JOIN role ";
+    query += "ON employee.role_id = role.id"
+    connection.query(query, function (err, result) {
+        if (err) throw err;
+
+        console.table(result)
+        startRun();
+    });
+};
+
+function departmentView(){
+    var query = "SELECT * FROM department";
+    connection.query(query, function(err, res){
+        console.table(res);
+        startRun();
+        //connection.end();
+        //quitApp();
+    });
+
+};
+
+
+function roleView(){
+    var query = "SELECT * FROM role";
+    connection.query(query, function(err, res){
+        console.table("View Roles :" + res);
+        startRun();
+    });
+};
+
+function updateEmployee(){
+    connection.query("SELECT * FROM role", function(err, res){
+
+        inquirer
+        .prompt([
+            {
+            name: "first_name", 
+            type: "input",
+            message: "Employee's first_name: ",
+           },
+           {
+            name: "last_name",
+            type: "input",
+            message: "Employee's last name: "
+           },{
+            name: "role",
+            type: "list",
+            choices: function(){
+                var arr =[];
+                for(var i =0; i<res.length; i++){
+                    arr.push(res[i].title);
+                }
+                return arr;
+            },
+            message: "What is the role ? " 
+        
+
+        }]).then(function(answer){
+            let roleID;
+            for(let a = 0; a<res.length; a++){
+                if(res[a].title === answer.role){
+                    roleID = res[a].id;
+                    console.log(roleID);
+                }
+            }
+            connection.query(
+                "INSERT INTO employee SET ?",{
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    role_id: answer.roleID,
+                },
+                function(err){
+                    if(err) throw err;
+                    console.log("NEW EMPLOYEE ADDED");
+                    startRun();
+                }
+            )
+        });
+    });
+}
+
+function quitApp(){
+    connection.end();
+};
+
